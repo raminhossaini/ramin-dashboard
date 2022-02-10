1b6b9415c8667e58495bd739e055e7ef5aeb8280
Ramin Hossaini <raminhossaini@users.noreply.github.com>
2022-02-10 09:51:54 +0200

    Updated Bootstrap Icons to v.1.8.1

--- app/Views/header.php
+++ app/Views/header.php
@@ -27,8 +27,7 @@
 	?>
 	
 	<!--<link href="<?=base_url();?>/includes/ticker.css" rel="stylesheet">-->
-	
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
 	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 <body>
